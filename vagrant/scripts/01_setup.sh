e460902d31ed3cf7410855cbb031e1d968a8edb7
NiBo <nibocn@gmail.com>
2020-09-27 16:08:16 +0800

    config(vagrant): 添加 vagrant 配置
    - 添加虚拟机分组配置；
    - 添加 Docker CE 阿里 yum 库；

---
+++ vagrant/scripts/01_setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "-----------------------------------------------------------------"
+echo -e "${INFO}`date +%F' '%T`: Setting-up /u01 disk"
+echo "-----------------------------------------------------------------"
+
+echo "Dcoker CE Repo URL: ${DOCER_CE_REPO_URL}"
